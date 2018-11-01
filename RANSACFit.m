6d296d3ef3251ee09862eed1ee8698d326c360aa
zafercavdar <zafercavdar@yahoo.com>
2018-11-02 00:46:15 +0300

    fixed wrong semicolon

--- RANSACFit.m
+++ RANSACFit.m
@@ -132,7 +132,7 @@ function H_best = RANSACFit(p1, p2, match, seedSampleSize, maxInlierError, goodF
         %                                                                 %
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         
-        number_of_inliers = size(inliers,1); + size(seed_group,1); 
+        number_of_inliers = size(inliers,1) + size(seed_group,1); 
         if( number_of_inliers > goodFitThresh )
 
 			% =================================================
@@ -149,6 +149,8 @@ function H_best = RANSACFit(p1, p2, match, seedSampleSize, maxInlierError, goodF
             
             p1_inliers = [p1(inliers(:, 1), :) ; p1(seed_group(:, 1), :)];
             p2_inliers = [p2(inliers(:, 2), :) ; p2(seed_group(:, 2), :)];
+            % p1_inliers = p1(inliers(:, 1), :);
+            % p2_inliers = p2(inliers(:, 2), :);
             H_candidate = ComputeAffineMatrix(p1_inliers, p2_inliers);
             fitting_err = norm(ComputeError(H_candidate, p1_inliers, p2_inliers));
             if fitting_err < min_error
